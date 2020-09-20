975bfacdc2833f152fee77235f3943e0a901a286
Evghenii Kunitski <kunitsky@gmail.com>
2020-09-20 16:43:34 +0200

    homework done

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -10,12 +10,27 @@
 
 """
 
+import csv
+
+future_table = [
+    {'name': 'Заповедникъ ЗОЖ', 'age': 40, 'job': 'Овоще база'},
+    {'name': 'Capaldi', 'age': 25, 'job': 'бот'},
+    {'name': 'Neznajka bot', 'age': 40, 'job': 'просто бот'},
+    {'name': 'Alienspain', 'age': 40, 'job': 'Писатель'},
+    {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+    {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
+    {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'}
+    ]
+
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open('table.csv', 'w', encoding='utf-8') as csv_file:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(csv_file, fields, delimiter=';')
+        writer.writeheader()
+        for line in future_table:
+            writer.writerow(line)
+
 
 if __name__ == "__main__":
     main()
